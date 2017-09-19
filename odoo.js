1a31c6f0646dd746bcc1a7b96bdc6f700e9428d5
Dinosys Dev <dinosys@tt.dinosys.vn>
2017-09-19 21:09:01 +0700

    Forces url to contain http(s) prefix.

--- odoo.js
+++ odoo.js
@@ -7,7 +7,7 @@ module.exports = Odoo;
 
 function authenticate(url, db, username, password) {
   return new Promise(function (resolve, reject) {
-    var client = new Client(`https://${url}/xmlrpc/2/common`);
+    var client = new Client(`${url}/xmlrpc/2/common`);
     client.call('authenticate', [db, username, password, {}], function (err, xml) {
       if (err) {
         reject(err);
@@ -28,7 +28,7 @@ function authenticate(url, db, username, password) {
 
 function execute_kw(url, db, uid, password, model, method, params, extra) {
   return new Promise(function (resolve, reject) {
-    var client = new Client(`https://${url}/xmlrpc/2/object`);
+    var client = new Client(`${url}/xmlrpc/2/object`);
     client.call('execute_kw', [db, uid, password,
       model, method, params, extra], function (err, xml) {
       if (err) {
@@ -48,58 +48,108 @@ function execute_kw(url, db, uid, password, model, method, params, extra) {
   });
 }
 
+/**
+ * @class Odoo
+ * @param {object} options The initial parameters
+ * , e,g { url: 'http(s)://...', db: 'demo_db', username: 'test_user', password: 'secret' } 
+ */
 function Odoo(options) {
     this.settings = options || {};
 }
 
+/**
+* Send authentication request to the server 
+*/
 Odoo.prototype.authenticate = async function () {
     return await authenticate(this.settings.url, this.settings.db, this.settings.username, this.settings.password);
 }
 
+/**
+ * Send authentication request to the server and catch the id of login user
+ */
 Odoo.prototype.doAuthentication = async function () {
     if (!this.settings.uid) {
-        console.log ('authenticating...');
         this.settings.uid = await this.authenticate();
     }
 }
 
+/**
+ * Search for the given model. It returns a list of ids
+ * @param {string} model The model name
+ * @param {array} params An array of filters (aka domain), e.g. [[['is_company', '=', True], ['customer', '=', True]]]
+ * @param {object} extra An extra json parameter, e.g. { fields: [], offset: 0, limit: 1000 }
+ */
 Odoo.prototype.search = async function (model, params, extra) {
     await this.doAuthentication();
     return await execute_kw(this.settings.url, this.settings.db, this.settings.uid, this.settings.password,
         model, "search", params, extra);
 }
 
+/**
+ * Count the number of records for the given model
+ * @param {string} model The model name
+ * @param {array} params An array of filters (aka domain), e.g. [[['is_company', '=', True], ['customer', '=', True]]]
+ */
 Odoo.prototype.search_count = async function (model, params) {
     await this.doAuthentication();
     return await execute_kw(this.settings.url, this.settings.db, this.settings.uid, this.settings.password,
         model, "search_count", params, {});
 }
 
+/**
+ * Read the detials for the given model
+ * @param {string} model The model name
+ * @param {array} params An array of filters (aka domain), e.g. [[['is_company', '=', True], ['customer', '=', True]]]
+ * @param {object} extra An extra json parameter, e.g. { fields: [], offset: 0, limit: 1000 }
+ */
 Odoo.prototype.read = async function (model, params, extra) {
     let ids = await this.search (model, params, extra);
     return await execute_kw(this.settings.url, this.settings.db, this.settings.uid, this.settings.password, model, "read", ids, {});
 }
 
+/**
+ * Get the properties of the given model
+ * @param {string} model The model name
+ * @param {object} attributes An extra json parameter, e.g. {'attributes': ['string', 'help', 'type']}
+ */
 Odoo.prototype.fields_get = async function (model, attributes) {
     await this.doAuthentication();
     return await execute_kw(this.settings.url, this.settings.db, this.settings.uid, this.settings.password, model, "fields_get", [], attributes);
 }
 
+/**
+ * An other implementation of read()
+ */
 Odoo.prototype.search_read = async function (model, params, extra) {
     await this.doAuthentication();
     return await execute_kw(this.settings.url, this.settings.db, this.settings.uid, this.settings.password, model, "search_read", params, extra);
 }
 
+/**
+ * Create a new record for the given model
+ * @param {string} model The model name
+ * @param {object} fields Details of the model
+ */
 Odoo.prototype.create = async function (model, fields) {
     await this.doAuthentication();
     return await execute_kw(this.settings.url, this.settings.db, this.settings.uid, this.settings.password, model, "create", [fields]);
 }
 
+/**
+ * Update a model for the given id
+ * @param {string} model The model name
+ * @param {object} fields Details of the model
+ */
 Odoo.prototype.update = async function (model, id, fields) {
     await this.doAuthentication();
     return await execute_kw(this.settings.url, this.settings.db, this.settings.uid, this.settings.password, model, "write", [[id], fields]);
 }
 
+/**
+ * Delete a model for the given id
+ * @param {string} model The model name
+ * @param {number} id The id of the model
+ */
 Odoo.prototype.delete = async function (model, id) {
     await this.doAuthentication();
     return await execute_kw(this.settings.url, this.settings.db, this.settings.uid, this.settings.password, model, "unlink", [[id]]);
